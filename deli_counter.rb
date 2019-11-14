0ba839d54ba257badaa88d6e30c472f93f33441a
Jordan Hayes <nosafoundation@gmail.com>
2019-11-14 22:11:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,6 +9,10 @@ def line(array)
     puts statement
   end
 end
-def take_a_number(array, string)
-  
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome #{name}. You are number #{array.index+1} in line."
+  return name, position
 end
