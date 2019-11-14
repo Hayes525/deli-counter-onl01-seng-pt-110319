198b6cda16b861f6f84d36a9912985c8502ed9d5
Jordan Hayes <nosafoundation@gmail.com>
2019-11-14 22:13:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,6 @@ end
 def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
-  puts "Welcome #{name}. You are number #{array.index+1} in line."
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
   return name, position
 end
